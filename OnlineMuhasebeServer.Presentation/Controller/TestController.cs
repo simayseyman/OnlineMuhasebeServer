af5de6ab97f6d6ecef087b6c95f8187510be6a79
simay <simay@SIMAY_LENOVO>
2024-09-25 14:31:06 +0300

    Add project files.

---
+++ OnlineMuhasebeServer.Presentation/Controller/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineMuhasebeServer.Presentation.Abstraction;
+
+namespace OnlineMuhasebeServer.Presentation.Controller
+{
+    public sealed class TestController : ApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("İşlem Başarılı");
+        }
+
+    }
+}
